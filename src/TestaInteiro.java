cc493c261a7c7fa908398971946586d31d4e4060
phmiranda <pehhagah.1607@gmail.com>
2019-04-04 06:08:32 -0300

    alterações necessários conforme aula.

--- src/TestaInteiro.java
+++ src/TestaInteiro.java
@@ -1,4 +1,4 @@
-public class Inteiro {
+public class TestaInteiro {
     // comentários
     public static void main(String args[]){
         // comentários
---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,28 @@
+public class TestaPontoFlutuante {
+    // comentários
+    public static void main(String args[]){
+        // comentários
+        double salario;
+
+        // comentários
+        salario = 1250.70;
+
+        // comentários
+        System.out.println("O meu salário é: " + salario);
+
+        // comentários
+        double idade = 37;
+
+        // comentários
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+
+        // comentários
+        int outraDivisao = 5/2;
+        System.out.println(outraDivisao);
+
+        // comentários
+        double novaTentativa = 5.0/2;
+        System.out.println(novaTentativa);
+    }
+}
