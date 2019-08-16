fc4ac72f96beffea0e3d0b75beab11ece9a1f2d2
phmiranda <phmiranda@stefanini.com>
2019-08-16 12:53:12 -0300

    [add] - inclusão do projeto utilizando maven

--- src/Programa.java
+++
@@ -1,8 +0,0 @@
-// comentários
-public class Programa {
-    // comentários
-    public static void main(String args[]){
-        // comentários
-        System.out.println("Ola mundo...");
-    }
-}
