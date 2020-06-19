691dda0fc152e79519689021c5c96eb610eccf08
pedro.miranda <pedro.miranda@defesa.gov.br>
2020-06-19 16:42:26 -0300

    [add] - criação do pacote referente a quarta parte do curso de exceções da carreira de desenvolvedor Java.

---
+++ src/main/java/br/com/alura/sintaxe_excecao/Fluxo.java
@@ -0,0 +1,30 @@
+/**
+ * Project:
+ * Date: 19/06/2020
+ * User: phmiranda
+ * Description: fluxo
+ */
+
+package br.com.alura.sintaxe_excecao;
+
+public class Fluxo {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
