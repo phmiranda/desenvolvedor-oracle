c1f4daf8529abe8f91809b4acce36f7bb7aa7817
C1299877 Pedro Henrique Marques Miranda <c1299877@interno.bb.com.br>
2020-10-27 09:53:03 -0300

    [update] - organização dos pacotes para cada parte da trilha de desenvolvimento Java.

---
+++ src/main/java/br/com/alura/sintaxe/basica/PrimitivoFloat.java
@@ -0,0 +1,14 @@
+/**
+ * Project: desenvolvedor
+ * Date: 09/06/2020
+ * User: phmiranda
+ * Descrição:
+ */
+
+package br.com.alura.sintaxe.basica;
+
+public class PrimitivoFloat {
+    public static void main(String[] args) {
+        float numero;
+    }
+}
