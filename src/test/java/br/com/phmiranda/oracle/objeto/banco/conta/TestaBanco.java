9f6d115a695b56c4295cd83462ef71d9ba6413c5
phmiranda <pehhagah.1607@gmail.com>
2022-01-17 12:31:56 -0300

    correção na estrutura de pacotes.
    correção na estrutura de pacotes.

--- src/test/java/br/com/phmiranda/oracle/objeto/banco/conta/TestaBanco.java
+++ src/test/java/br/com/phmiranda/oracle/objeto/banco/conta/TestaBanco.java
@@ -5,7 +5,7 @@
  * Description: N/A
  * Date: 29/09/2021
  */
-package br.com.phmiranda.oracle.objeto.banco;
+package br.com.phmiranda.oracle.objeto.banco.conta;
 
 public class TestaBanco {
     // COMMENT
---
+++ src/test/java/br/com/phmiranda/oracle/objeto/banco/herdado/TestaMetodo.java
@@ -0,0 +1,12 @@
+/*
+ * Author: phmiranda
+ * Project: desenvolvedor-oracle-se
+ * Task Number: HUXXX
+ * Description: N/A
+ * Date: 17/01/2022
+ */
+
+package br.com.phmiranda.oracle.objeto.banco.herdado;
+
+public class TestaMetodo {
+}
